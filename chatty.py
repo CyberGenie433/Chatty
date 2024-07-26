a37722ce3b7b3d96e68c6e422aa2a5f5a1324ad7
Theandre Smith <atlanticwavecouriers@gmail.com>
2024-07-26 15:03:12 -0400

    Update chatty.py

--- chatty.py
+++ chatty.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+st.title("_Streamlit_ is :red[cool] :sunglasses:")
 
 name = st.text_input("Enter your name:")
+
+st.caption("This is a string that explains something above.")
+st.caption("A caption with _italics_ :black[colors] and emojis :sunglasses:")
