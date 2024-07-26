5ec6e5b66f8e35a4e3dfdb009a5ab96ebe72595b
Theandre Smith <atlanticwavecouriers@gmail.com>
2024-07-26 14:49:03 -0400

    Update chatty.py

--- chatty.py
+++ chatty.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-st.title("Welcome to My First Streamlit App")
+st.title("Welcome to My First Streamlit App","_Streamlit_ is :blue[cool] :sunglasses:")
 
 name = st.text_input("Enter your name:")
 
-st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 
 
 
