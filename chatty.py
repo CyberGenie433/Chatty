a239242cbd8223be02a36c3c2cbc3c304c9ba31d
Theandre Smith <atlanticwavecouriers@gmail.com>
2024-07-26 15:11:48 -0400

    Update chatty.py

--- chatty.py
+++ chatty.py
@@ -4,15 +4,3 @@ st.title("_Streamlit_ is :red[cool] :sunglasses:")
 
 name = st.text_input("Enter your name:")
 
-st.json(
-    {
-        "foo": "bar",
-        "baz": "boz",
-        "stuff": [
-            "stuff 1",
-            "stuff 2",
-            "stuff 3",
-            "stuff 5",
-        ],
-    }
-)
