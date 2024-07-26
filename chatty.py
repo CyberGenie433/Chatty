1632e2d3ec264c31f6ef43ebd64257d241c84ffb
Theandre Smith <atlanticwavecouriers@gmail.com>
2024-07-26 14:42:02 -0400

    Update chatty.py

--- chatty.py
+++ chatty.py
@@ -4,14 +4,3 @@ st.title("Welcome to My First Streamlit App")
 
 name = st.text_input("Enter your name:")
 
-md = st.text_area('Type in your markdown string (without outer quotes)',
-                  "Happy Streamlit-ing! :balloon:")
-
-st.code(f"""
-import streamlit as st
-
-st.markdown('''{md}''')
-""")
-
-st.markdown(md)
-
