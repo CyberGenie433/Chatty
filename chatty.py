ab501450786bde66cce68c3f8da7b4b201b2c997
Theandre Smith <atlanticwavecouriers@gmail.com>
2024-07-26 15:00:30 -0400

    Update chatty.py

--- chatty.py
+++ chatty.py
@@ -3,10 +3,3 @@ import streamlit as st
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 
 name = st.text_input("Enter your name:")
-
-st.header("This is a header with a divider", divider="gray")
-st.header("These headers have rotating dividers", divider=True)
-st.header("One", divider=True)
-st.header("Two", divider=True)
-st.header("Three", divider=True)
-st.header("Four", divider=True)
