f7c25075f6c0f6fdeed8c093769b16719520fd66
Theandre Smith <atlanticwavecouriers@gmail.com>
2024-07-26 15:11:07 -0400

    Update chatty.py

--- chatty.py
+++ chatty.py
@@ -3,3 +3,16 @@ import streamlit as st
 st.title("_Streamlit_ is :red[cool] :sunglasses:")
 
 name = st.text_input("Enter your name:")
+
+st.json(
+    {
+        "foo": "bar",
+        "baz": "boz",
+        "stuff": [
+            "stuff 1",
+            "stuff 2",
+            "stuff 3",
+            "stuff 5",
+        ],
+    }
+)
