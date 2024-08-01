db893c7b322c473d98ce8d6e7bfbdcbbb099d789
Theandre Smith <atlanticwavecouriers@gmail.com>
2024-08-01 14:57:46 -0400

    Update chatty.py

--- chatty.py
+++ chatty.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("_Streamlit_ is :red[cool] :sunglasses:")
 name = str.text_input("Enter a name:")
-st.write("Hello,",name,"how are you doing?")
+st.write("Hello,",name,"how are you doing?
 
